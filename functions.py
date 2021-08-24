89512cd7d8fac5f233fb7112912ff3ce4a34ceb4
Rolowy <rolowaty@gmail.com>
2021-08-24 16:27:42 +0200

    cleaning

--- functions.py
+++ functions.py
@@ -1,11 +1,4 @@
-from flask import Flask, jsonify
-from markupsafe import escape
 from requests_html import HTMLSession
-
-app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-jsonlist  = []
 
 def refresh(url):
     session = HTMLSession()
@@ -18,12 +11,5 @@ def refresh(url):
     for i in range(len(name)):
         jsonlist.append({"name":name[i].text,
         "price":price[i].text.replace("zł", "zl")})
+
     session.close()
-
-for i in range(1,6):
-    refresh("?page="+str(i))
-
-@app.route("/")
-def index():
-    print(jsonlist)
-    return jsonify(keys=jsonlist)
